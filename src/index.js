d750b18da0e5f8f5ea406630a54733af713cb4e3
orumant <orumant.a@ya.ru>
2019-02-24 03:15:03 +0300

    initial commit * настроен redux * добавлен material-ui

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import {mainReducer} from "./modules/mainReducer";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+const store = createStore(
+    mainReducer,
+    composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
+
+serviceWorker.unregister();
