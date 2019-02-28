35f96e127bbe019e5c097d3843068cbb01c3c87a
Orumant <asp.w@inbox.ru>
2019-02-28 13:44:54 +0300

    + added mainPage

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MainPage from "./pages/MainPage";
+import LoginModal from "./pages/MainPage/index/LoginModal";
 
-it('renders without crashing', () => {
+it(`renders App without crashing`, () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders LoginModal without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<LoginModal />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders MainPage without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<MainPage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
